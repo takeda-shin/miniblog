c418b7107427355e1cf10693fbb4a732b49f63c0
takeda-shin <kgcfy958@yahoo.co.jp>
2019-09-27 19:59:09 +0900

    Add model article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_27_102050) do
+ActiveRecord::Schema.define(version: 2019_09_27_105359) do
+
+  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -20,4 +28,5 @@ ActiveRecord::Schema.define(version: 2019_09_27_102050) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "articles", "users"
 end
